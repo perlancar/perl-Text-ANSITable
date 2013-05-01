570cb2be37aad8050e67ba99054507edd88b303c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-05-01 13:15:49 +0700

    Use gradation border color by default

--- lib/Text/ANSITable/ColorTheme/Default.pm
+++ lib/Text/ANSITable/ColorTheme/Default.pm
@@ -4,6 +4,8 @@ use 5.010;
 use strict;
 use warnings;
 
+use SHARYANTO::Color::Util qw(mix_2_rgb_colors);
+
 # VERSION
 
 our %color_themes = (
@@ -15,16 +17,44 @@ our %color_themes = (
         no_color => 1,
     },
 
-    default_16 => {
-        summary => 'Default for 16-color terminal',
-        colors => {
-        },
-    },
+    #default_16 => {
+    #    summary => 'Default for 16-color terminal',
+    #    colors => {
+    #    },
+    #},
 
-    default_256 => {
-        summary => 'Default for 256-color terminal (black background)',
+    #default_256 => {
+    default => {
+        summary => 'Default for 256-color+ terminal (black background)',
+        description => <<'_',
+
+Border color has gradation from top to bottom. Accept arguments C<border1> and
+C<border2> to set first (top) and second (bottom) foreground RGB colors. Colors
+will fade from the top color to bottom color. Also accept C<border1_bg> and
+C<border2_bg> to set background RGB colors.
+
+_
         colors => {
-            border      => '666666',
+            border      => sub {
+                my ($self, %args) = @_;
+
+                my $pct = ($self->{_draw}{y}+1) / $self->{_draw}{table_height};
+
+                my $rgbf1 = $self->{color_theme_args}{border1} // 'ffffff';
+                my $rgbf2 = $self->{color_theme_args}{border2} // '444444';
+                my $rgbf  = mix_2_rgb_colors($rgbf1, $rgbf2, $pct);
+
+                my $rgbb1 = $self->{color_theme_args}{border1_bg};
+                my $rgbb2 = $self->{color_theme_args}{border2_bg};
+                my $rgbb;
+                if ($rgbb1 && $rgbb2) {
+                    $rgbb = mix_2_rgb_colors($rgbb1, $rgbb2, $pct);
+                }
+
+                #say "D:$rgbf, $rgbb";
+                [$rgbf, $rgbb];
+            },
+
             cell_bg     => '',
 
             num_data    => '',
